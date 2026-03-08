9173e6affbc94aea33d408910c3e19f2a216abcc
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2026-03-08 19:00:49 +0530

    Huristic constraints function update

--- PlanogramPlatform/backend/src/app.js
+++ PlanogramPlatform/backend/src/app.js
@@ -28,6 +28,7 @@ import externalFactorsRoutes from './routes/externalFactors.routes.js';
 import inventoryRoutes from './routes/inventory.routes.js';
 import complianceRoutes from './routes/compliance.routes.js';
 import scenarioRoutes from './routes/scenarioRoutes.js';
+import constraintRoutes from './routes/constraintRoutes.js';
 
 app.use('/api/stores', storeRoutes);
 app.use('/api/products', productRoutes);
@@ -42,6 +43,7 @@ app.use('/api/promotions', promotionRoutes);
 app.use('/api/planograms', planogramRoutes);
 app.use('/api/compliance', complianceRoutes);
 app.use('/api/scenario', scenarioRoutes);
+app.use('/api/constraints', constraintRoutes);
 
 
 
